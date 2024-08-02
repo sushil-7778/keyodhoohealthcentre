c8543b7df5f5ce481d0733f46dda717f18ee3368
Sushil <sushil7778@yahoo.com>
2024-08-02 07:03:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Medicine from './pages/MedicineList';
+
+//pages
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        
+        <Medicine/>
       </header>
-    </div>
+       
+                    
+    </div>    
+      
+
   );
 }
 
